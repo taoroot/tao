504d0ae90f6df74babbbad799ae28d6529dde6af
zhiyi <zy@flizi.cn>
2020-08-09 23:35:01 +0800

    fix bug

--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -60,9 +60,11 @@
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
 
       <div class="tips">
-        <a referrerpolicy="origin" href="https://api.flizi.cn/tao/oauth2/authorization/github">GitHub | </a>
-        <a referrerpolicy="origin" href="https://api.flizi.cn/tao/oauth2/authorization/gitea">GITEA | </a>
-        <a referrerpolicy="origin" href="https://api.flizi.cn/tao/oauth2/authorization/gitee">GITEE | </a>
+        <el-row style="text-align: center;">
+          <el-col span="8"><a referrerpolicy="origin" href="https://api.flizi.cn/tao/oauth2/authorization/gitee"> 码云 </a></el-col>
+          <el-col span="8"><a referrerpolicy="origin" href="https://api.flizi.cn/tao/oauth2/authorization/github"> GitHub </a></el-col>
+          <el-col span="8"><a referrerpolicy="origin" href="https://api.flizi.cn/tao/oauth2/authorization/gitea"> GITEA </a></el-col>
+        </el-row>
       </div>
 
     </el-form>
