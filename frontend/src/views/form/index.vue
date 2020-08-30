eb05f076dbe2ed79633728d1d73cac00eb57f7aa
zhiyi <zy@flizi.cn>
2020-08-30 19:44:24 +0800

    fix bug

--- frontend/src/views/form/index.vue
+++ frontend/src/views/form/index.vue
@@ -4,7 +4,7 @@
     <h1>1.点击选择按钮选择Excel文件; 2.点击上传按钮 3. 稍等片刻,转换成功自动下载zip文件</h1>
 
     <form enctype="multipart/form-data" method="post" action="http://api.flizi.cn/tj/upload">
-      <input type="file" name="file" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel">
+      <input type="file" name="file">
       <input type="submit" value="上传">
     </form>
   </div>
